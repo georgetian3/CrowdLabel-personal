00b898a4e1e131323428c6092303021522bba190
George Tian <github@georgetian.com>
2022-10-15 11:54:35 +0800

    host 0.0.0.0

--- src/app.py
+++ src/app.py
@@ -40,4 +40,4 @@ def about():
     return 'Software Engineering 2022 fall group project'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
