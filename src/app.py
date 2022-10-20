ee668a0d521d29cee8af7d74b24a9b481338d465
chenjz20 <843273746@qq.com>
2022-10-20 15:58:16 +0800

    emailSender

--- src/app.py
+++ src/app.py
@@ -1,28 +1,33 @@
 from flask import Flask, request, render_template, redirect
 from crowdlabel import CrowdLabel
-
+from emailSender import EmailSender
 server = CrowdLabel()
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/home')
 def home():
     return redirect('/')
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        print('json', request.get_json(force=True))
+        sender = EmailSender()
+        print('json', request.get_data())
         return 'test'
         # TODO: handle user login
         pass
 
-@app.route('/register', methods = ['GET', 'POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
 
@@ -31,13 +36,16 @@ def register():
         # TODO: handle user registration
         pass
 
+
 @app.route('/admin')
 def admin():
     return 'admin'
 
+
 @app.route('/about')
 def about():
     return 'Software Engineering 2022 fall group project'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
