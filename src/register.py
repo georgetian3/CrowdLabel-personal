f9fecd0ad7c46d0e781d0bf846b13adf3870ef25
George Tian <github@georgetian.com>
2022-10-25 16:18:40 +0800

    requirement doc register, login, checkers

--- src/register.py
+++ src/register.py
@@ -1,9 +1,24 @@
+from ast import Pass
+from checkers.user import check_username
 from config import conn
+from checkers.user import *
+
+from argon2 import PasswordHasher
+hasher = PasswordHasher()
 
 cur = conn.cursor()
 
+def register(username, password, email):
 
-def add_user(username, password):
+    if not check_username(username):
+        return 'username'
+    if not check_password(password):
+        return 'password'
+    if not check_email(email):
+        return 'email'
+
+    password = hasher.hash(password)
+
     sql = "INSERT INTO user(id, username, password) VALUES ('%s','%s','%s')" % (
         0, username, password)
 
@@ -11,3 +26,5 @@ def add_user(username, password):
     # commit
     conn.commit()  # 对数据库内容有改变，需要commit()
     conn.close()
+
+    return 'ok'
