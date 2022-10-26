5601e76c2c19738dfe015419d48140e870d796c7
chenjz20 <843273746@qq.com>
2022-10-26 22:04:38 +0800

    sqlalchemy

--- src/register.py
+++ src/register.py
@@ -1,11 +1,16 @@
 from checkers.user import check_username
-from config import conn
 from checkers.user import *
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from database import User
 from argon2 import PasswordHasher
 hasher = PasswordHasher()
 
-cur = conn.cursor()
+#cur = conn.cursor()
+
+engine = create_engine(
+    "mysql+pymysql://root:cxq1974328@127.0.0.1:3306/crowdlabel?charset=utf8")
+Connection = sessionmaker(bind=engine)
 
 
 def register_add_user(username, password, usertype, email, status):
@@ -20,11 +25,17 @@ def register_add_user(username, password, usertype, email, status):
         return 'email'
     print(password)
     password = hasher.hash(password)
-    sql = "INSERT INTO user(username, password, usertype,email,status) VALUES ('%s','%s','%s','%s','%s')" % (
-        username, password, usertype, email, status)
-    cur.execute(sql)
+    con = Connection()
+    user = User(username=username, password=password,
+                email=email, type=usertype, status=status)
+    con.add(user)
+    con.commit()
+    con.close()
+    # sql = "INSERT INTO user(username, password, usertype,email,status) VALUES ('%s','%s','%s','%s','%s')" % (
+    #    username, password, usertype, email, status)
+    # cur.execute(sql)
     # commit
-    conn.commit()  # 对数据库内容有改变，需要commit()
-    conn.close()
+    # conn.commit()  # 对数据库内容有改变，需要commit()
+    # conn.close()
     print("ok")
     return 'ok'
