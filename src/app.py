e39a37a6457aeedcee0df2d838c215037e2095ab
George Tian <github@georgetian.com>
2022-10-29 09:58:35 +0800

    port correction

--- src/app.py
+++ src/app.py
@@ -95,6 +95,6 @@ def about():
 
 if __name__ == '__main__':
     if platform.system() == 'Linux':
-        app.run(debug=False, host='0.0.0.0', port=80)
+        app.run(debug=False, host='0.0.0.0', port=8000)
     else:
         app.run(debug=True, host='127.0.0.1', port=8000)
