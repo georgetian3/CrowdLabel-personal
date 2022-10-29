dbdc6852fcdab21c0f85bb925116aa2ec7a1ccc6
George Tian <github@georgetian.com>
2022-10-29 13:11:29 +0800

    email use outlook

--- src/emailSender.py
+++ src/emailSender.py
@@ -2,25 +2,20 @@ from typing import Iterable
 from flask import Flask
 import flask_mail
 
+from datetime import datetime
 
 
 # 异步发送邮件
 class EmailSender:
     def __init__(self):
         self.app = Flask(__name__)
-        # SMTP服务器地址，例如QQ邮箱的smtp.qq.com
-        self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-        # SMTP服务器端口，一般为465
+        self.app.config['MAIL_SERVER'] = 'smtp.office365.com'
         self.app.config['MAIL_PORT'] = 587
-        # 是否启用SSL加密
         self.app.config['MAIL_USE_SSL'] = False
-        # 是否启用TLS加密
         self.app.config['MAIL_USE_TLS'] = True
-        self.app.config['MAIL_USERNAME'] = 'crowdlabel.org@gmail.com'
-        # 授权码，在设置smtp的时候有
+        self.app.config['MAIL_USERNAME'] = 'crowdlabel.org@outlook.com'
         with open('email.key') as f:
             self.app.config['MAIL_PASSWORD'] = f.read().strip()
-        # 初始化对象
         self.mail_obj = flask_mail.Mail(self.app)
 
     def send_email(self,
@@ -47,7 +42,7 @@ if __name__ == '__main__':
     sender = EmailSender()
     sender.send_email(
         'test subject',
-        'test body',
+        str(datetime.now()),
         'noreply@crowdlabel.org',
         ['me@georgetian.com']
     )
